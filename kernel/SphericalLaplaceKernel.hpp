205edb4a7a870ffe592bf79eb2f13d90ef16c18d
Cris Cecka <ccecka@seas.harvard.edu>
2012-09-18 21:41:15 -0400

    Removed Kernel-setDomain

--- kernel/SphericalLaplaceKernel.hpp
+++ kernel/SphericalLaplaceKernel.hpp
@@ -67,38 +67,6 @@ class SphericalLaplaceKernel
   //! Get radius of root cell
   real getR0() const {return R0;}
 
-  //! Set center and size of root cell
-  void setDomain(Bodies &bodies, vect x0=0, real r0=M_PI) {
-    vect xmin,xmax;                                             // Min,Max of domain
-    B_iter B = bodies.begin();                                  // Reset body iterator
-    xmin = xmax = B->X;                                         // Initialize xmin,xmax
-    for( B=bodies.begin(); B!=bodies.end(); ++B ) {             // Loop over bodies
-      for( int d=0; d!=3; ++d ) {                               //  Loop over each dimension
-        if     (B->X[d] < xmin[d]) xmin[d] = B->X[d];           //   Determine xmin
-        else if(B->X[d] > xmax[d]) xmax[d] = B->X[d];           //   Determine xmax
-      }                                                         //  End loop over each dimension
-    }                                                           // End loop over bodies
-    if( IMAGES != 0 ) {                                         // If periodic boundary condition
-      if( xmin[0] < x0[0]-r0 || x0[0]+r0 < xmax[0]              //  Check for outliers in x direction
-          || xmin[1] < x0[1]-r0 || x0[1]+r0 < xmax[1]              //  Check for outliers in y direction
-          || xmin[2] < x0[2]-r0 || x0[2]+r0 < xmax[2] ) {          //  Check for outliers in z direction
-        std::cout << "Error: Particles located outside periodic domain : " << std::endl;// Print error message
-        std::cout << xmin << std::endl;
-        std::cout << xmax << std::endl;
-      }                                                         //  End if for outlier checking
-      X0 = x0;                                                  //  Center is [0, 0, 0]
-      R0 = r0;                                                  //  Radius is r0
-    } else {
-      for( int d=0; d!=3; ++d ) {                               // Loop over each dimension
-        X0[d] = (xmax[d] + xmin[d]) / 2;                        // Calculate center of domain
-        X0[d] = int(X0[d]+.5);                                  //  Shift center to nearest integer
-        R0 = std::max(xmax[d] - X0[d], R0);                     //  Calculate max distance from center
-        R0 = std::max(X0[d] - xmin[d], R0);                     //  Calculate max distance from center
-      }                                                         // End loop over each dimension
-      R0 *= 1.000001;                                           // Add some leeway to root radius
-    }                                                           // Endif for periodic boundary condition
-  }
-
   //! Precalculate M2L translation matrix
   void preCalculation() {
     printf("PreCalculation starting\n");

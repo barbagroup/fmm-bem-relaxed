ea22a4549455e890e825c229a4f33841e5b1bc60
Cris Cecka <ccecka@seas.harvard.edu>
2012-10-01 16:08:24 -0400

    Fixed TransformIterator, added test

--- include/TransformIterator.hpp
+++ include/TransformIterator.hpp
@@ -1,18 +1,23 @@
 #pragma once
 
+#include <iterator>
 #include <type_traits>
+#include <utility>
+
+// Automatically derive !=, <=, >, and >= from a class's == and <
+using namespace std::rel_ops;
 
 template <class UnaryFunction,
           class Iterator>
 class transform_iterator
 {
 public:
-  typedef std::result_of<const UnaryFunction(iterator_traits<Iterator>::reference)>::type reference;
-  typedef std::remove_cv<std::remove_reference<reference>>::type value_type;
-  typedef std::add_pointer<value_type>::type pointer;
+  typedef typename std::result_of<const UnaryFunction(typename std::iterator_traits<Iterator>::reference)>::type reference;
+  typedef typename std::remove_cv<std::remove_reference<reference>>::type value_type;
+  typedef typename std::add_pointer<value_type>::type pointer;
 
-  typedef iterator_traits<Iterator>::difference_type difference_type;
-  typedef iterator_traits<Iterator>::iterator_category iterator_category;
+  typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
+  typedef typename std::iterator_traits<Iterator>::iterator_category iterator_category;
 
   // CONSTRUCTORS
 
@@ -43,13 +48,24 @@ public:
     return f_(*it_);
   }
   transform_iterator& operator++() {
-    return ++it_;
+    ++it_;
+    return *this;
   }
   transform_iterator& operator--() {
-    return --it_;
+    --it_;
+    return *this;
+  }
+  bool operator==(const transform_iterator& it) const {
+    return it_ == it.it_;
   }
 
 private:
   Iterator it_;
   UnaryFunction f_;
 };
+
+
+template <typename IT, class F>
+transform_iterator<F,IT> make_transform_iter(IT it, F f) {
+  return transform_iterator<F,IT>(it, f);
+}

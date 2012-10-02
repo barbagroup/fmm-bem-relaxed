abb847c7d802cf0f1f6c84478327ab53c833ca11
Cris Cecka <ccecka@seas.harvard.edu>
2012-10-01 23:30:39 -0400

    Removed Bloat

--- include/SimpleEvaluator.hpp
+++ include/SimpleEvaluator.hpp
@@ -41,9 +41,11 @@ public:
   typedef typename Kernel::charge_type charge_type;
 
 private:
-  // kernel & expansions
-  Kernel &K;
+  //! Kernel
+  Kernel& K;
+  //! Multipole expansions corresponding to Box indices in Octree
   std::vector<multipole_type> M;
+  //! Local expansions corresponding to Box indices in Octree
   std::vector<local_type> L;
 
 public:
@@ -190,143 +192,4 @@ public:
       }
     }
   }
-
-  //! Approximate interaction between two cells
-  inline void approximate(C_iter Ci, C_iter Cj) {
-#if HYBRID
-    if( timeP2P*Cj->NDLEAF < timeM2P && timeP2P*Ci->NDLEAF*Cj->NDLEAF < timeM2L) {// If P2P is fastest
-      evalP2P(Ci,Cj);                                           //  Evaluate on CPU, queue on GPU
-    } else if ( timeM2P < timeP2P*Cj->NDLEAF && timeM2P*Ci->NDLEAF < timeM2L ) {// If M2P is fastest
-      evalM2P(Ci,Cj);                                           //  Evaluate on CPU, queue on GPU
-    } else {                                                    // If M2L is fastest
-      evalM2L(Ci,Cj);                                           //  Evaluate on CPU, queue on GPU
-    }                                                           // End if for kernel selection
-#elif TREECODE
-    evalM2P(Ci,Cj);                                             // Evaluate on CPU, queue on GPU
-    //K.M2P(*Cj,M[Cj->ICELL],*Ci);
-#else
-    evalM2L(Ci,Cj);                                             // Evalaute on CPU, queue on GPU
-#endif
-  }
-
-
-  //! Use multipole acceptance criteria to determine whether to approximate, do P2P, or subdivide
-  void interact(C_iter Ci, C_iter Cj, PairQueue& pairQueue) {
-    vect dX = Ci->X - Cj->X - Xperiodic;                        // Distance vector from source to target
-    real Rq = std::sqrt(norm(dX));                              // Scalar distance
-    if( Rq * THETA > Ci->R + Cj->R ) {                          // If distance if far enough
-      approximate(Ci,Cj);                                       //  Use approximate kernels, e.g. M2L, M2P
-    } else if(Ci->NCHILD == 0 && Cj->NCHILD == 0) {             // Else if both cells are leafs
-      evalP2P(Ci,Cj);                                           //  Use P2P
-    } else {                                                    // If cells are close but not leafs
-      Pair pair(Ci,Cj);                                         //  Form a pair of cell iterators
-      pairQueue.push_back(pair);                                //  Push pair to queue
-    }                                                           // End if for multipole acceptance
-  }
-
-  //! Dual tree traversal
-  void traverse(Cells& cells, Cells& jcells) {
-    C_iter root = cells.end() - 1;                              // Iterator for root target cell
-    C_iter jroot = jcells.end() - 1;                            // Iterator for root source cell
-    Ci0 = cells.begin();                                        // Set begin iterator for target cells
-    Cj0 = jcells.begin();                                       // Set begin iterator for source cells
-    // Non-periodic
-    Iperiodic = Icenter;                                      //  Set periodic image flag to center
-    Xperiodic = 0;                                            //  Set periodic coordinate offset
-    Pair pair(root,jroot);                                    //  Form pair of root cells
-    traverseQueue(pair);                                      //  Traverse a pair of trees
-  }
-
-  //! Traverse a pair of trees using a queue
-  void traverseQueue(Pair pair) {
-    PairQueue pairQueue;                                        // Queue of interacting cell pairs
-    pairQueue.push_back(pair);                                  // Push pair to queue
-    while( !pairQueue.empty() ) {                               // While dual traversal queue is not empty
-      pair = pairQueue.front();                                 //  Get interaction pair from front of queue
-      pairQueue.pop_front();                                    //  Pop dual traversal queue
-      if(splitFirst(pair.first,pair.second)) {                  //  If first cell is larger
-        // Cj->NCHILD == 0 || (Ci->NCHILD != 0 && Ci->R > Cj->R)
-        C_iter C = pair.first;                                  //   Split the first cell
-        for( C_iter Ci=Ci0+C->CHILD; Ci!=Ci0+C->CHILD+C->NCHILD; ++Ci ) {// Loop over first cell's children
-          interact(Ci,pair.second,pairQueue);                   //    Calculate interaction between cells
-        }                                                       //   End loop over fist cell's children
-      } else {                                                  //  Else if second cell is larger
-        C_iter C = pair.second;                                 //   Split the second cell
-        for( C_iter Cj=Cj0+C->CHILD; Cj!=Cj0+C->CHILD+C->NCHILD; ++Cj ) {// Loop over second cell's children
-          interact(pair.first,Cj,pairQueue);                    //    Calculate interaction betwen cells
-        }                                                       //   End loop over second cell's children
-      }                                                         //  End if for which cell to split
-    }                                                           // End while loop for dual traversal queue
-  }
-
-  //! Downward phase (M2L,M2P,P2P,L2L,L2P evaluation)
-  void downward(Cells& cells, Cells& jcells, bool periodic=true) {
-    Log.startTimer("Traverse");                                     // Start timer
-    traverse(cells,jcells);                                     // Traverse tree to get interaction list
-    Log.stopTimer("Traverse");                             // Stop timer & print
-
-    evalL2L();
-    evalL2P();
-  }
-
-  static void evalP2P(const Kernel& K, Bodies& ibodies, Bodies& jbodies) {
-    Xperiodic = 0;                                                // Set periodic coordinate offset
-    Cells cells;                                                  // Cells to put target and source bodies
-    cells.resize(2);                                              // Resize cells to put target and source bodies
-    cells[0].LEAF = ibodies.begin();                              // Iterator of first target leaf
-    cells[0].NDLEAF = ibodies.size();                             // Number of target leafs
-    cells[1].LEAF = jbodies.begin();                              // Iterator of first source leaf
-    cells[1].NDLEAF = jbodies.size();                             // Number of source leafs
-    C_iter Ci = cells.begin(), Cj = cells.begin()+1;              // Iterator of target and source cells
-    printf("evaluating %d x %d P2P\n",(int)ibodies.size(),(int)jbodies.size());
-    K.P2P(Ci,Cj);
-  }
 };
-
-template <class Kernel>
-void Evaluator<Kernel>::evalP2P(Bodies& ibodies, Bodies& jbodies, bool) {// Evaluate all P2P kernels
-  evalP2P(K, ibodies, jbodies); // Perform P2P kernel
-}
-
-template <class Kernel>
-void Evaluator<Kernel>::evalM2L(C_iter Ci, C_iter Cj) {       // Evaluate single M2L kernel
-  vect translation = Ci->X - Cj->X;
-  K.M2L(M[Cj->ICELL],L[Ci->ICELL],translation);
-  NM2L++;                                                       // Count M2L kernel execution
-}
-
-template <class Kernel>
-void Evaluator<Kernel>::evalM2P(C_iter Ci, C_iter Cj) {       // Evaluate single M2P kernel
-  // K.M2P(*Cj,M[Cj->ICELL],*Ci);
-  K.M2P(Cj->X,M[Cj->ICELL],*Ci);
-  NM2P++;                                                       // Count M2P kernel execution
-}
-
-template <class Kernel>
-void Evaluator<Kernel>::evalP2P(C_iter Ci, C_iter Cj) {       // Evaluate single P2P kernel
-  K.P2P(Ci,Cj);                                                   // Perform P2P kernel
-  NP2P++;                                                       // Count P2P kernel execution
-}
-
-template <class Kernel>
-void Evaluator<Kernel>::evalL2L(Cells &cells) {               // Evaluate all L2L kernels
-  // for each cell in range, get parent & perform L2L
-  for( C_iter Ci=cells.end()-2; Ci!=cells.begin()-1; --Ci )
-  {
-    C_iter parent = cells.begin()+Ci->PARENT;
-    vect dist = Ci->X-parent->X;
-    K.L2L(L[parent->ICELL],L[Ci->ICELL],dist);
-  }
-}
-
-template <class Kernel>
-void Evaluator<Kernel>::evalL2P(Cells &cells) {               // Evaluate all L2P kernels
-  Log.startTimer("evalL2P");                                        // Start timer
-  for( C_iter C=cells.begin(); C!=cells.end(); ++C ) {          // Loop over cells
-    if( C->NCHILD == 0 ) {                                      //  If cell is a twig
-      K.L2P(*C,L[C->ICELL]);
-    }                                                           //  Endif for twig
-  }                                                             // End loop over cells topdown
-  Log.stopTimer("evalL2P");                                         // Stop timer
-}
-
